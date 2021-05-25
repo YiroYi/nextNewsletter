18c07d1b87c3f7e5b4cbbe38bb8c3a80c61721e2
YiroYi <yiroyi@live.com>
2021-05-25 01:05:51 -0500

    Adding MongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient} from 'mongodb';
+
+async function handler(req, res) {
   if(req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,8 +8,17 @@ function handler(req, res) {
       res.status(422).json({message: 'Invalid email'});
       return;
     }
+    console.log(process.env.DB_USER)
+    console.log(process.env.DB_PASSWORD)
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ejjn0.mongodb.net/newsletter?retryWrites=true&w=majority`)
 
-    console.log(userEmail)
+    const db = client.db();
+
+    await db.collection('emails').insertOne({email: userEmail});
+
+    client.close();
+
     res.status(200).json({message: 'Signed up'});
   }
 }
